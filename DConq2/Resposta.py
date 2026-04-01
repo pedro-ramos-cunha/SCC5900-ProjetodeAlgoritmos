3490a01f486529fb00f07814c0dc5e0c10c68b1e
[PRCUNHA] Pedro Ramos Cunha <pedro.ramos@embraer.com.br>
2026-03-31 22:29:53 -0300

    Cleanning copy paste codes inside files copy from previous works

--- DConq2/Resposta.py
+++ DConq2/Resposta.py
@@ -1,43 +1,6 @@
 #################################################################
-## Tarefa 7
+## Tarefa 9
 ## Nome: Pedro ramos Cunha
 ## NUSP: 10892248
 ## Curso: PPG-CCMC-ICMC-USP Mestrado em Ciência de Computação 
-## Data de Submissão: 22/03/2026 13:10:18
-
-#################################################################
-## Checa se a rainha está em uma posição válida 
-## Não pode estar na mesma linha (condição 1)
-## Ou na mesma coluna (condição 2)
-## Ou na mesma diagonal (condição 3) -- Esta ultima é indicada pela diferencça igual entre x0-x1 e y0-y1)
-
-def check_if_valid(queens, new_queen):
-    for queen in queens:
-        if queen[0] == new_queen[0] or queen[1] == new_queen[1] or abs(queen[0] - new_queen[0]) == abs(queen[1] - new_queen[1]):
-            return False
-    return True
-
-
-
-
-def backtracking_queens(slots_burned,queens_placed ,index):
-    cases_success = 0
-    for i in range(8):
-        if check_if_valid(queens_placed, (index, i)) and (index, i) not in slots_burned:
-            if index == 7:
-                return 1
-
-            queens_placed.append((index, i))
-            cases_success += backtracking_queens(slots_burned,queens_placed, index + 1)
-            queens_placed.pop()
-    return cases_success
-
-slots_burned = list()
-for i in range(8):
-    linha_tabuleiro = list(input())
-    for j in range(8):
-        if linha_tabuleiro[j] == '*':
-            slots_burned.append((i, j))
-
-cases_success = backtracking_queens(slots_burned,[], 0)
-print(cases_success)
+## Data de Submissão: 
--- DConq2/Resposta_data.md
+++ DConq2/Resposta_data.md
@@ -5,24 +5,11 @@
 |Curso: PPG-CCMC-ICMC-USP Mestrado em Ciência de Computação  |
 |Nome: Pedro ramos Cunha                            |
 |NUSP: 10892248 |
-|Data de Submissão: 22/03/2026 13:10:18 |
+|Data de Submissão:  |
 
 Informações acerca dos resultados obtidos
 
 ## Dados da Execução
-
-|   Caso  |  Status | Tempo de CPU | Tam. de Memória Utilizado |     Mensagem     |
-|:-------:|:-------:|:------------:|:-------------------------:|:----------------:|
-|  Caso 1 | Correto |   0.0262 s   |           -1 Kb           | Resposta Correta |
-|  Caso 2 | Correto |   0.0261 s   |           -1 Kb           | Resposta Correta |
-|  Caso 3 | Correto |   0.0275 s   |           -1 Kb           | Resposta Correta |
-|  Caso 4 | Correto |   0.0215 s   |           -1 Kb           | Resposta Correta |
-|  Caso 5 | Correto |   0.9887 s   |           -1 Kb           | Resposta Correta |
-|  Caso 6 | Correto |   0.0204 s   |           -1 Kb           | Resposta Correta |
-|  Caso 7 | Correto |   0.0187 s   |           -1 Kb           | Resposta Correta |
-|  Caso 8 | Correto |   0.0198 s   |           -1 Kb           | Resposta Correta |
-|  Caso 9 | Correto |   0.0189 s   |           -1 Kb           | Resposta Correta |
-| Caso 10 | Correto |   0.0178 s   |           -1 Kb           | Resposta Correta |
 
 
 ## Informações sobre a complexidade
