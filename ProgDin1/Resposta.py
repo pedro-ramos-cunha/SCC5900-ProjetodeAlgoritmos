b18dc1673291f169a32f7b18c5940677f16c9eaa
[PRCUNHA] Pedro Ramos Cunha <pedro.ramos@embraer.com.br>
2026-04-11 15:33:15 -0300

    Limpeza do template

--- ProgDin1/Resposta.py
+++ ProgDin1/Resposta.py
@@ -1,30 +1,6 @@
 #################################################################
-## Tarefa 8
+## Tarefa 10
 ## Nome: Pedro ramos Cunha
 ## NUSP: 10892248
 ## Curso: PPG-CCMC-ICMC-USP Mestrado em Ciência de Computação 
-## Data de Submissão: 01/04/2026 23:15:58
-
-t = int(input())
-
-
-def exponenciacao(base, exp, mod):
-    resultado = 1
-    base = base % mod
-    
-    while exp > 0:
-        # Se o expoente for ímpar, multiplicamos o resultado pela base atual
-        if exp % 2 == 1:
-            resultado = (resultado * base) % mod
-            
-        # Elevamos a base ao quadrado
-        base = (base * base) % mod
-        
-        # Dividimos o expoente por 2 (usamos // para divisão inteira em Python)
-        exp = exp // 2
-        
-    return resultado
-
-for _ in range(t):
-    car, c = map(int, input().split())       
-    print(exponenciacao(car, c, 1000000007))
+## Data de Submissão: 
--- ProgDin1/Resposta_data.md
+++ ProgDin1/Resposta_data.md
@@ -1,22 +1,15 @@
 # Informações sobre tempo de execução da tarefa
 
-|Tarefa 8                                        |
+|Tarefa 10                                        |
 |:------|
 |Curso: PPG-CCMC-ICMC-USP Mestrado em Ciência de Computação  |
 |Nome: Pedro ramos Cunha                            |
 |NUSP: 10892248 |
-|Data de Submissão: 01/04/2026 23:15:58|
+|Data de Submissão: |
 
 Informações acerca dos resultados obtidos
 
 ## Dados da Execução
-
-|  Caso  |  Status | Tempo de CPU | Tam. de Memória Utilizado |     Mensagem     |
-|:------:|:-------:|:------------:|:-------------------------:|:----------------:|
-| Caso 1 | Correto |   0.1878 s   |           -1 Kb           | Resposta Correta |
-| Caso 2 | Correto |   0.0397 s   |           -1 Kb           | Resposta Correta |
-| Caso 3 | Correto |   0.0287 s   |           -1 Kb           | Resposta Correta |
-
 
 ## Informações sobre a complexidade
 
